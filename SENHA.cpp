d40b4dd19491e58a48faef21812b7ed737f02765
Jhennifer Ferreira <jhennifer.ferreira@escola.pr.gov.br>
2025-09-26 11:47:08 -0300

    Create SENHA.cpp

---
+++ SENHA.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+  int main ()
+  {
+    int senha=0; // criando um variavel de senha
+    printf("digite uma senha:\n"); // pedindo para digitar
+    scanf("%d", &senha); // digitando uma senha
+    
+    // repetir ate que tentativa seja < 10000 ou seja 9999.
+    for(int i = 0; i < 10; i++){
+        for(int x = 0; x<10; x++){
+    for(int w = 0;w<10;w++){
+                for(int z = 0;z<10;z++)
+            printf("valor: %d %d %d %d \n", i,x,w,z);
+        }
+        }
+    }
+  }
